e6fdc2d5a0546b35c141c5982fad3f63eae66608
Mrigank Tandon <sachitkapoor01@gmail.com>
2020-06-14 02:24:35 +0530

    Logout menu added

--- app/src/main/java/com/example/baateinkaro/MainActivity.java
+++ app/src/main/java/com/example/baateinkaro/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.baateinkaro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
@@ -47,5 +51,28 @@ public class MainActivity extends AppCompatActivity {
     private void SendUserToLoginActivity() {
         Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
         startActivity(loginIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.options_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+         super.onOptionsItemSelected(item);
+         if(item.getItemId()==R.id.logout){
+           mAuth.signOut();
+           SendUserToLoginActivity();
+         }
+         if(item.getItemId()==R.id.settings){
+
+         }
+         if(item.getItemId()==R.id.find_friends){
+
+         }
+         return true;
     }
 }
