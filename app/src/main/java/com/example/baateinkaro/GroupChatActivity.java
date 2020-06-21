0874ae6a67396cc4aa182cfaf7c5e47183be9630
Mrigank Tandon <sachitkapoor01@gmail.com>
2020-06-22 01:03:29 +0530

    setting title as grp title

--- app/src/main/java/com/example/baateinkaro/GroupChatActivity.java
+++ app/src/main/java/com/example/baateinkaro/GroupChatActivity.java
@@ -8,6 +8,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 
@@ -17,18 +18,21 @@ public class GroupChatActivity extends AppCompatActivity {
     private EditText usermessageinput;
     private ScrollView mscrollview;
     private TextView displayTextmessage;
+    private String currentgrpname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_chat);
+        currentgrpname=getIntent().getExtras().get("groupname").toString();
+        Toast.makeText(this, currentgrpname, Toast.LENGTH_SHORT).show();
         InitializeFields();
     }
 
     private void InitializeFields() {
         mtoolbar=(Toolbar) findViewById(R.id.grp_app_bar);
         setSupportActionBar(mtoolbar);
-        getSupportActionBar().setTitle("Group Name");
+        getSupportActionBar().setTitle(currentgrpname);
         sendmessagebutton=(ImageButton) findViewById(R.id.send_message);
         usermessageinput=findViewById(R.id.group_message);
         displayTextmessage=findViewById(R.id.text_grp);
--- app/src/main/java/com/example/baateinkaro/GroupFragment.java
+++ app/src/main/java/com/example/baateinkaro/GroupFragment.java
@@ -1,5 +1,6 @@
 package com.example.baateinkaro;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -46,6 +48,15 @@ public class GroupFragment extends Fragment {
         grpref= FirebaseDatabase.getInstance().getReference().child("Groups");
         Initializefields();
         retrieveanddisplay();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String currentgroup=parent.getItemAtPosition(position).toString();
+                Intent grpchat=new Intent(getContext(),GroupChatActivity.class);
+                grpchat.putExtra("groupname",currentgroup);
+                startActivity(grpchat);
+            }
+        });
         return  groupfragment;
     }
 
